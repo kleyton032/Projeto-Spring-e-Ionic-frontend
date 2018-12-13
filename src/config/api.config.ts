596bf441d0e2afa8e74e4ffc691b2330db5024a8
kleyton032 <kleyton.joao@gmail.com>
2018-12-13 18:02:14 -0200

    Mostrando as categorias

--- src/config/api.config.ts
+++ src/config/api.config.ts
@@ -1,3 +1,4 @@
 export const API_CONFIG = {
-    baseUrl:  "http://localhost:8080"
+    baseUrl:  "http://localhost:8080",
+    baseUrlBucket: "https://s3-sa-east-1.amazonaws.com/ionic-app-spring"
 }
