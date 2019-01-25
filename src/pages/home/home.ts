45af889b67348390d3905470d75fd58e98f5524a
kleyton032 <kleyton.joao@gmail.com>
2019-01-25 19:50:17 -0200

    Comecando a implementar a autenticacao

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,3 +1,4 @@
+import { AuthService } from './../../services/auth.service';
 import { CredenciaisDTO } from './../../models/credenciais.dto';
 import { CategoriasPage } from './../categorias/categorias';
 import { Component } from '@angular/core';
@@ -15,7 +16,9 @@ export class HomePage {
     senha: ""
   }
 
-  constructor(public navCtrl: NavController, public menu: MenuController) {
+  constructor(public navCtrl: NavController, 
+    public menu: MenuController,
+    public auth: AuthService) {
 
   }
 
@@ -30,8 +33,12 @@ export class HomePage {
   }
 
   login() {
-    console.log(this.creds)
-    this.navCtrl.setRoot("CategoriasPage")
+    this.auth.authenticate(this.creds).subscribe(response=>{
+      console.log(response.headers.get("Authorization")),
+      this.navCtrl.setRoot("CategoriasPage")
+    },
+    error => {}); 
+    
   }
 
 }
